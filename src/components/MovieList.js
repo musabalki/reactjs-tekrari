8f18c9e23ada7af8f2e7ddc8d8531a6562f1408a
musabalki <musabalkimb@gmail.com>
2021-03-23 21:37:28 +0300

    Card oluşturuldu

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -3,7 +3,23 @@ import React from "react"
 class MovieList extends React.Component {
     render(){
         return (
-            <h1>Movie List</h1>
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-4">
+                        <div className="card mb-4 shadow-sm">
+                            <img  alt="" className="card-img-top" src="https://image.tmdb.org/t/p/w600_and_h900_bestv2/4hne3v6jN4MlCnhSkxOW7YspJhr.jpg" />
+                            <div className="card-body">
+                                <h5 className="card-title">Sample Movie</h5>
+                                <p className="card-text">Sample Movie</p>
+                                <div className="d-flex justify-content-between align-items-center">
+                                    <button type="button" className="btn btn-md btn-outline-danger">Delete</button>
+                                    <h2><span className="bg-dark text-white pe-2 ps-2">9.0</span></h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
